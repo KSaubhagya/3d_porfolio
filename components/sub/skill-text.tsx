47ac304719abc02a2ad576f1e856ea518ed8c46c
KSaubhagya <saubhagyakrhmk.21@uom.lk>
2026-05-07 21:26:58 +0530

    Optimize code

--- components/sub/skill-text.tsx
+++ components/sub/skill-text.tsx
@@ -31,13 +31,6 @@ export const SkillText = () => {
           Skills and Technologies
         </h2>
       </motion.div>
-
-      {/* <motion.div
-        variants={slideInFromRight(0.5)}
-        className="cursive text-[20px] text-gray-200 mb-10 mt-[10px] text-center"
-      >
-        Never miss a task, deadline or idea.
-      </motion.div> */}
     </div>
   );
 };
